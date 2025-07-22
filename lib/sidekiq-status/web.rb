9472bca24307e985286b215cc887a08ec2a2c9f6
ihor <isliusar@liaisonedu.com>
2025-07-22 14:07:20 +0300

    sidekiq 8 support

--- lib/sidekiq-status/web.rb
+++ lib/sidekiq-status/web.rb
@@ -1,14 +1,11 @@
+require_relative 'web/helpers'
 # adapted from https://github.com/cryo28/sidekiq_status
 
 module Sidekiq::Status
   # Hook into *Sidekiq::Web* Sinatra app which adds a new "/statuses" page
   module Web
-    # Location of Sidekiq::Status::Web view templates
-    VIEW_PATH = File.expand_path('../../../web/views', __FILE__)
-
     DEFAULT_PER_PAGE_OPTS = [25, 50, 100].freeze
     DEFAULT_PER_PAGE = 25
-    COMMON_STATUS_HASH_KEYS = %w(update_time jid status worker args label pct_complete total at message working_at elapsed eta)
 
     class << self
       def per_page_opts= arr
@@ -33,76 +30,16 @@ module Sidekiq::Status
 
     # @param [Sidekiq::Web] app
     def self.registered(app)
-
-      # Allow method overrides to support RESTful deletes
-      app.set :method_override, true
-
-      app.helpers do
-        def csrf_tag
-          "<input type='hidden' name='authenticity_token' value='#{session[:csrf]}'/>"
-        end
-
-        def poll_path
-          "?#{request.query_string}" if params[:poll]
-        end
-
-        def sidekiq_status_template(name)
-          path = File.join(VIEW_PATH, name.to_s) + ".erb"
-          File.open(path).read
-        end
-
-        def add_details_to_status(status)
-          status['label'] = status_label(status['status'])
-          status["pct_complete"] ||= pct_complete(status)
-          status["elapsed"] ||= elapsed(status).to_s
-          status["eta"] ||= eta(status).to_s
-          status["custom"] = process_custom_data(status)
-          return status
-        end
-
-        def process_custom_data(hash)
-          hash.reject { |key, _| COMMON_STATUS_HASH_KEYS.include?(key) }
-        end
-
-        def pct_complete(status)
-          return 100 if status['status'] == 'complete'
-          Sidekiq::Status::pct_complete(status['jid']) || 0
-        end
-
-        def elapsed(status)
-          case status['status']
-          when 'complete'
-            Sidekiq::Status.update_time(status['jid']) - Sidekiq::Status.working_at(status['jid'])
-          when 'working', 'retrying'
-            Time.now.to_i - Sidekiq::Status.working_at(status['jid'])
-          end
-        end
-
-        def eta(status)
-          Sidekiq::Status.eta(status['jid']) if status['status'] == 'working'
-        end
-
-        def status_label(status)
-          case status
-          when 'complete'
-            'success'
-          when 'working', 'retrying'
-            'warning'
-          when 'queued'
-            'primary'
-          else
-            'danger'
-          end
-        end
-
-        def has_sort_by?(value)
-          ["worker", "status", "update_time", "pct_complete", "message", "args"].include?(value)
-        end
-      end
+      app.helpers Web::Helpers
 
       app.get '/statuses' do
         @workers = Set.new(['all'])
-        jids = Sidekiq::WorkSet.new.map {|process_id, thread_id, work|  JSON.parse(work['payload'])['jid'] }
+
+        jids = Sidekiq::WorkSet.new.map do |process_id, thread_id, work|
+          payload = Sidekiq::MAJOR >= 8 ? work.payload : work['payload']
+          JSON.parse(payload)['jid']
+        end
+
         @statuses = []
 
         jids.each do |jid|
@@ -155,7 +92,8 @@ module Sidekiq::Status
       end
 
       app.get '/statuses/:jid' do
-        job = Sidekiq::Status::get_all params['jid']
+        jid = Sidekiq::MAJOR >= 8 ? route_params('jid') : params['jid']
+        job = Sidekiq::Status::get_all jid
 
         if job.empty?
           throw :halt, [404, {"Content-Type" => "text/html"}, [erb(sidekiq_status_template(:status_not_found))]]
@@ -187,13 +125,29 @@ unless defined?(Sidekiq::Web)
   require 'sidekiq/web'
 end
 
-Sidekiq::Web.register(Sidekiq::Status::Web)
+if Sidekiq::MAJOR >= 8
+  Sidekiq::Web.configure do |config|
+    config.register_extension(
+      Sidekiq::Status::Web,
+      name: "statuses",
+      tab: ["Statuses"],
+      index: "statuses"
+    )
+  end
+elsif Sidekiq::MAJOR > 6
+  Sidekiq::Web.configure do |config|
+    config.register(Sidekiq::Status::Web, name: "statuses", tab: ["Statuses"], index: "statuses")
+  end
+else
+  Sidekiq::Web.register(Sidekiq::Status::Web)
+  if Sidekiq::Web.tabs.is_a?(Array)
+    # For sidekiq < 2.5
+    Sidekiq::Web.tabs << "statuses"
+  else
+    Sidekiq::Web.tabs["Statuses"] = "statuses"
+  end
+end
+
 ["per_page", "sort_by", "sort_dir", "status", "worker"].each do |key|
   Sidekiq::WebHelpers::SAFE_QPARAMS.push(key)
-end
-if Sidekiq::Web.tabs.is_a?(Array)
-  # For sidekiq < 2.5
-  Sidekiq::Web.tabs << "statuses"
-else
-  Sidekiq::Web.tabs["Statuses"] = "statuses"
 end
